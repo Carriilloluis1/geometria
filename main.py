adc9097b59287676c0cd79e10ac970664e3abc65
Carrilloluis1 <barajavalencia@gmail.com>
2024-09-16 15:37:38 -0300

    main modificado

--- main.py
+++ main.py
@@ -3,14 +3,25 @@ import paquete_geometria.perimetros as paqper
 
 # Calculo de Areas
 
-print(paqarea.calcular_area_circulo())
-paqarea.calcular_area_cuadrado(25)
-paqarea.calcular_area_triangulo(20,10.2)
+area_circulo = paqarea.calcular_area_circulo()
+print(area_circulo)
+
+area_cuadrado = paqarea.calcular_area_cuadrado(25)
+print(area_cuadrado)
+
+area_triangulo = paqarea.calcular_area_triangulo(20,10.2)
+print(area_triangulo)
+
 
 #Calculo Perímetros
 
-paqper.calcular_perimetro_circulo(8)
-paqper.calcular_perimetro_cuadrado(20)
-paqper.calcular_perimetro_triangulo(15,50,10)
+perimetro_circulo = paqper.calcular_perimetro_circulo(8)
+print(perimetro_circulo)
+
+perimetro_cuadrado = paqper.calcular_perimetro_cuadrado(20)
+print(perimetro_cuadrado)
+
+perimetro_triangulo = paqper.calcular_perimetro_triangulo(15,50,10)
+print(perimetro_triangulo)
 
 
